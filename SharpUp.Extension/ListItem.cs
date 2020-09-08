6408a7e536b0350f18c6ce0df3631ddb1b8bb53d
Nam Hoang <namht.pyn@gmail.com>
2020-09-08 07:55:08 +0700

    Commit

---
+++ SharpUp.Extension/ListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpUp.Extension
+{
+    public class ListItem<T1, T2>
+    {
+        public ListItem(T1 value, T2 text)
+        {
+            Value = value;
+            Text = text;
+        }
+
+        public T1 Value { get; set; }
+        public T2 Text { get; set; }
+    }
+}
